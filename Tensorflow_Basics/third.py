581f4744b7305d32534bc10b6ef4ab289cc887cd
KnightBea <88288922+KnightBea@users.noreply.github.com>
2022-07-16 12:51:25 +0530

    Did the third program

--- Tensorflow_Basics/third.py
+++ Tensorflow_Basics/third.py
@@ -1,20 +1,33 @@
 '''Something'''
+
 import tensorflow as tf
-import numpy as np
-from tensorflow import keras
+print(tf.__version__)
 
-#Defines the shape of the model
-model = tf.keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])])
+#Inheritng the callback function
+class myCallback(tf.keras.callbacks.Callback):
+  def on_epoch_end(self, epoch, logs={}):
+    if(logs.get('accuracy')>0.90):
+      print("\nReached 90% accuracy so cancelling training!")
+      self.model.stop_training = True
 
-#Defines what type of calculations we want on the model
-model.compile(optimizer='sgd',loss = 'mean_squared_error')
+callbacks = myCallback()
+mnist = tf.keras.datasets.fashion_mnist
+(training_images, training_labels), (test_images, test_labels) = mnist.load_data()
+training_images = training_images.reshape(60_000,28,28,1)
+training_images=training_images/255.0
+test_images = test_images.reshape(10_000,28,28,1)
+test_images=test_images/255.0
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Conv2D(64,(3,3),activation='relu',input_shape=(28,28,1)),  #Adds the convolution Layer which extract the necessary components from the required image
+  tf.keras.layers.MaxPooling2D(2,2),  #Adds the Pooling layer which Decreaseds the size of the training data's pixels
+  tf.keras.layers.Flatten(),
+  tf.keras.layers.Dense(256, activation=tf.nn.relu),
+  tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.summary() #Gives the summary of the model i.e the history of the modification to the model
+model.fit(training_images, training_labels, epochs=10, callbacks=[callbacks])
 
-#The data we use to train our model
-xs = np.array([-3.0,-2.0,-1.0,0.0,1.0,2.0,3.0,4.0,5.0,6.0,7.0],dtype=float)
-ys = np.array([-8.0,-5.0,-2.0,1.0,4.0,7.0,10.0,13.0,16.0,19.0,22.0],dtype=float)
-
-#The trainin of our model
-model.fit(xs,ys,epochs=500)
-
-#Printing the prediction made by our model after learning from the data we provided and asking for an output for a new input
-print(model.predict([10.0]))
+classifications =  model.predict(training_images)
+print(classifications[0])
+print(training_labels[0])
